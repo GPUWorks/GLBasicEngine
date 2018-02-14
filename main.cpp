5389b19fad5e734f77262f23d09cfaf29b3b5c16
lukehayes <lukepurplehayes@gmail.com>
2018-02-14 18:15:46 +0000

    Added shaderProgram getter

--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ int main()
         // clear the buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glUseProgram(sp.program);
+        glUseProgram(sp.getShaderProgram());
         // draw...
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
