79e47bc3239d6662fa14d9e5d94b734951c43050
lukehayes <lukepurplehayes@gmail.com>
2018-02-14 18:39:14 +0000

    Added comments to shaderProgram

--- include/shaderProgram.h
+++ include/shaderProgram.h
@@ -10,9 +10,18 @@ namespace Shader
         ShaderProgram();
         ~ShaderProgram();
 
-
+        /**
+         * Load the shader
+         * @param  vertex_file_path   Vertex Shader Path
+         * @param  fragment_file_path Fragment Shader Path
+         */
         GLuint loadShaders(const char * vertex_file_path,const char * fragment_file_path);
+
+        /**
+         * Get the shader program
+         */
         GLuint getShaderProgram() const;
+
 
     private:
         GLuint program;
