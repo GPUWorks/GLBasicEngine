1242e9cf2492e7752c4e2e1a0a90ad54ba54cb67
lukehayes <lukepurplehayes@gmail.com>
2018-02-16 18:45:31 +0000

    Corrected vertex shader

--- assets/vsh.glsl
+++ assets/vsh.glsl
@@ -5,6 +5,6 @@ uniform mat4 transform;
 
 
 void main(){
-    gl_Position.xyz = transform * vec4(vertexPosition_modelspace, 1.0f);
-    gl_Position.w = 1.0; 
+    gl_Position = transform * vec4(vertexPosition_modelspace, 1.0f);
+    // gl_Position.w = 1.0; 
 }
