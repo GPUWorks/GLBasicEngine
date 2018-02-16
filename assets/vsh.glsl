a2e2be35d8f5513e28d10b0c3b19b60200b9b39b
lukehayes <lukepurplehayes@gmail.com>
2018-02-16 18:44:00 +0000

    Updated shader

--- assets/vsh.glsl
+++ assets/vsh.glsl
@@ -1,8 +1,10 @@
 #version 330 core
 layout(location = 0) in vec3 vertexPosition_modelspace;
 
+uniform mat4 transform;
+
 
 void main(){
-    gl_Position.xyz = vertexPosition_modelspace;
+    gl_Position.xyz = transform * vec4(vertexPosition_modelspace, 1.0f);
     gl_Position.w = 1.0; 
 }
