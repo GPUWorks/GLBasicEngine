ff11940017d27122f1aec7ef67404032b867d655
lukehayes <lukepurplehayes@gmail.com>
2018-02-17 11:15:31 +0000

    Removed uniform variable from vertex shader

--- assets/vsh.glsl
+++ assets/vsh.glsl
@@ -1,10 +1,7 @@
 #version 330 core
 layout(location = 0) in vec3 vertexPosition_modelspace;
 
-uniform mat4 transform;
-
-
 void main(){
-    gl_Position = transform * vec4(vertexPosition_modelspace, 1.0f);
+    gl_Position = vec4(vertexPosition_modelspace, 1.0f);
     // gl_Position.w = 1.0; 
 }
