901a272e80aafb44c23646e455340db8bac59795
lukehayes <lukepurplehayes@gmail.com>
2018-02-17 11:44:12 +0000

    Updated main code

--- main.cpp
+++ main.cpp
@@ -110,8 +110,8 @@ int main()
         std::cout << delta << std::endl;
 
         glm::mat4 trans;
-        trans = glm::translate(trans, glm::vec3(0.1f, -0.5f, -5.0f));
-        trans = glm::rotate(trans, delta, glm::vec3(0.0f, 0.0f, 1.0f));
+        trans = glm::translate(trans, glm::vec3(0.1f, -0.5f, std::sin(delta) * 10));
+        trans = glm::rotate(trans, delta, glm::vec3(0.4f, 0.8f, 1.0f));
 
 
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);  
