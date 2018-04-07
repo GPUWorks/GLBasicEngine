4b48ceb5652faf11b40dbe260bcac0e9fea809d9
lukehayes <lukepurplehayes@gmail.com>
2018-04-07 11:27:14 +0100

    Added basic camera movement

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -1,4 +1,6 @@
 #include "camera/camera.h"
+#include <SFML/Window.hpp>
+#include <iostream>
 
 Camera::Camera(){}
 Camera::~Camera(){}
@@ -16,6 +18,19 @@ void Camera::setup() {
 }
 
 void Camera::update(float delta, Shader::ShaderProgram& sp) {
+
+    setup();
+    sf::Mouse mouse;
+    sf::Vector2i mpos = mouse.getPosition();
+
+    glm::vec3 front;
+    front.x = cos(glm::radians((float)mpos.x)) * cos(glm::radians((float)mpos.y));
+    front.y = sin(glm::radians((float)mpos.y));
+    front.z = sin(glm::radians((float)mpos.x)) * cos(glm::radians((float)mpos.y));
+    this->cameraFront = glm::normalize(front);
+
+    std::cout << mpos.x << std::endl;
+
     unsigned int viewProjLoc = glGetUniformLocation(sp.getShaderProgram(), "view");
     glUniformMatrix4fv(viewProjLoc, 1, GL_FALSE, glm::value_ptr(this->viewProjection));
 }
