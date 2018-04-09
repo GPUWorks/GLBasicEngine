be68d0d3a1b3b805c5fb35a52fb861c225f23e0b
lukehayes <lukepurplehayes@gmail.com>
2018-04-09 10:16:29 +0100

    Added camera key movement

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -22,6 +22,23 @@ void Camera::update(float delta, Shader::ShaderProgram& sp) {
     setup();
     sf::Mouse mouse;
     sf::Vector2i mpos = mouse.getPosition();
+    float cameraSpeed = 0.4;
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        cameraPos += cameraSpeed * cameraFront;
+    }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
 
     glm::vec3 front;
     front.x = cos(glm::radians((float)mpos.x)) * cos(glm::radians((float)mpos.y));
